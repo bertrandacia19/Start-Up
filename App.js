dcda3610c8fd3b4a1d512560c87f7f0b0bb2e755
Rocio9416 <72575074+Rocio9416@users.noreply.github.com>
2020-12-14 16:57:41 -0600

    Update App.js
    arreglo de app.js

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  View } from 'react-native';
 import * as SplashScreen from "expo-splash-screen";
+import { NavigationContainer }from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
 import useDatabase from "./src/hoock/useDataBase";
-import { DramaContextProvider } from "./src/context/Dramacontext";
+import  { DramaContextProvider } from "./src/context/Dramacontext";
+import  DramaListScreen  from "./src/Screens/DramaListScreen";
+//import { StackActions } from '@react-navigation/native';
+//import { ScreenStack } from 'react-native-screens';
 
+
+const Stack = createStackNavigator();
 export default function App() {
-   // Prevenir que la pantalla de splash se oculte
+  // Prevenir que la pantalla de splash se oculte
   SplashScreen.preventAutoHideAsync();
 
   const isLoadingComplete = useDatabase();
 
+
   // Ocultar la pantalla de splash
   if (isLoadingComplete) SplashScreen.hideAsync();
-  
   return (
-    <View style={styles.container}>
-        <DramaContextProvider>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <DramaContextProvider>
+        <NavigationContainer>
+          <Stack.Screen name= "ListScreen" component ={DramaListScreen} />
+        </NavigationContainer>
+        
       </DramaContextProvider>
     </View>
   );
